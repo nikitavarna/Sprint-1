c813a9b1bb470abdb8b130a9426c4a303cd56c30
nikitavarna <nikitasvarna@gmail.com>
2021-10-22 21:46:46 +0530

    err: KeyboardArrowRight-Icon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,64 @@
 import React, { useState } from "react";
-import List from "./List";
 import ListStyle from './ListStyle.css';
 import "./App.css";
+import Button from '@material-ui/core/esm/Button'; 
+import Typography from '@material-ui/core/esm/Typography';
+import TextField from "@material-ui/core/esm/TextField";
+//import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
+import { makeStyles } from '@material-ui/core/esm/styles';
+import { createMuiTheme,ThemeProvider } from "@material-ui/core/esm/styles";
+
+const useStyles=makeStyles((theme) => {
+  return({
+    submitButton: {
+      padding: '3px',
+      margin: '5px'
+    },
+    clearButton: {
+      padding: '3px',
+      marginTop: '50px'
+    },
+    itemButtons : {
+
+    }
+  });
+});
+const themeX = createMuiTheme({
+  palette: {
+    type: "dark",
+    primary:{
+      main: '#42a5f5'
+    },
+    secondary:{
+      main: '#90caf9'
+    }
+  }
+});
 
 function App() {
+  const classes = useStyles();
+
+  const List = ({ items, deleteItem, editItem }) => {
+    return (
+      <div className="items">
+        {items.map((item) => {
+          const { id, title } = item;
+          return (
+            <div className="item">
+              <div className="itemName" key={id}>
+              {title}
+              </div>
+              <div className="buttonContainer">
+                <button type="button" className="classes.itemButtons" onClick={() => editItem(id)}>Edit</button>
+                <button type="button" className="classes.itemButtons" onClick={() => deleteItem(id)}>Delete</button>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   const [name, setName] = useState("");
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
@@ -45,22 +100,22 @@ function App() {
   };
 
   return (
+    <ThemeProvider theme={themeX}>
     <div className="list">
       <form className="userInput" onSubmit={handleSubmit}>
-          <input type="text" className="input" placeholder="Item" value={name} onChange={(e) => setName(e.target.value)} />
-          <button type="submit" className="add">
-            {isEditing ? "Edit" : "Submit"}
-          </button>
+          <TextField id="outlined-basic" variant="outlined" margin="dense" className="input" placeholder="Item" value={name} onChange={(e) => setName(e.target.value)} color="success" focused />
+          <Button variant="contained" type="submit" className={classes.submitButton} color="primary">
+            {isEditing ? "Edit" : "Submit"} 
+          </Button>
       </form>
       {list.length > 0 && (
         <div className="listItems">
           <List items={list} deleteItem={deleteItem} editItem={editItem} />
-          <button className="clear" onClick={clearList}>
-            Clear items
-          </button>
+          <Button variant="contained" className={classes.clearButton} onClick={clearList} color="secondary">Clear</Button>
         </div>
       )}
     </div>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import List from './List';
+//import List from './List';
 
 ReactDOM.render(
   <div>
